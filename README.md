9e4cb1fd4cf0a283db0bbfa08c51d3fa82fc3c24
demi.jiang <demi.jiang@myob.com>
2020-05-31 18:14:52 +1000

    Start the scala project

---
+++ README.md
@@ -0,0 +1,5 @@
+##Hello Http4s
+Follow Heaton's path to learn Http4s with Doobie-Postgresql
+```shell script
+Reference: https://github.com/heaton/hello-http4s
+```
---
+++ src/test/scala/me/demi/hello/http/HelloHttp4sSpec.scala
@@ -0,0 +1,11 @@
+package me.demi.hello.http
+
+import cats.effect.IO
+import org.http4s.Method.GET
+import org.http4s._
+import org.http4s.implicits._
+import org.specs2.mutable.Specification
+
+class HelloHttp4sSpec extends {
+
+}
